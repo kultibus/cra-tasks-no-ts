76de30bd0761a62adbf24e4b65b7374e7f35efbb
kultibus <bastumpo@gmail.com>
2024-02-08 22:35:22 +0800

    deploy

--- src/components/UI/icons/Cross.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./cross.module.scss";
-
-const Cross = () => {
-  return <span className={styles.cross}></span>;
-};
-
-export default Cross;
--- src/components/Warning/Warning.jsx
+++
@@ -1,9 +0,0 @@
-import cn from "classnames";
-import { useSelector } from "../../hooks/useSelector";
-import styles from "./warning.module.scss";
-
-export const Warning = ({ children }) => {
-  const opacity = useSelector(styles.show);
-
-  return <div className={cn(styles.warning, opacity)}>{children}</div>;
-};
